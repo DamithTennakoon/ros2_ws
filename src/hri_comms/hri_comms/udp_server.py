a62944e4cccf953dd1c01399662085d4f466b460
DamithTennakoon <88393680+DamithTennakoon@users.noreply.github.com>
2024-10-17 10:56:16 -0400

    ip update

--- src/hri_comms/hri_comms/udp_server.py
+++ src/hri_comms/hri_comms/udp_server.py
@@ -14,7 +14,7 @@ class UdpServer(Node):
         self._tx_data = "ROS server"
         self._buffer_size = 1024
         self._server_port = 2222
-        self._server_ip = '130.63.212.249'
+        self._server_ip = '130.63.213.25'
         self._client_ip = []
 
         # Initialize UDP server
