4e3160b75351ef416600a0aa3b22d6e8c018b5f1
DamithTennakoon <88393680+DamithTennakoon@users.noreply.github.com>
2024-11-15 14:08:40 -0500

    PWM

--- src/actuators/scripts/servo_controller.py
+++ src/actuators/scripts/servo_controller.py
@@ -9,25 +9,20 @@ GPIO.setmode(GPIO.BCM) # BCM: Broadcom SOC channel
 GPIO.setup(servoGpioPin, GPIO.OUT)
 
 pin = GPIO.PWM(servoGpioPin, 50) # Set GPIO pin 17 to operate at 50Hz 
-pin.start(2.5) # Duty cycle for MG996R servo - units ms
+pin.start(0) # Duty cycle for MG996R servo - units ms
 try:
     while True:
-        #pin.ChangeDutyCycle(5) # 0
-        #time.sleep(2)
-        #pin.ChangeDutyCycle(7.5)
-        #time.sleep(2)
-        #pin.ChangeDutyCycle(10) #-90
-        #time.sleep(2)
-        pin.ChangeDutyCycle(5) # 0
+        pin.ChangeDutyCycle(1.5) # 0
+        time.sleep(2)
+        pin.ChangeDutyCycle(2) # 90
+        time.sleep(2)
+        pin.ChangeDutyCycle(1.5) #0
+        time.sleep(2)
+        pin.ChangeDutyCycle(1) # -90
         time.sleep(2)
 except KeyboardInterrupt:
     pin.stop()
     GPIO.cleanup()
-
-
-# 0 = -90 pos
-# 10 = 0 pos
-#
 
 '''
 servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
