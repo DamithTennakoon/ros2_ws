0301822c8f8da084b228c6613c7c45a19f00fbf1
DamithTennakoon <88393680+DamithTennakoon@users.noreply.github.com>
2024-11-15 13:21:16 -0500

    Testing PWM command for servo controller

--- src/actuators/scripts/servo_controller.py
+++ src/actuators/scripts/servo_controller.py
@@ -1,8 +1,30 @@
 # Import RPi libraries
-import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO # For PWM
 import time
-from gpiozero import AngularServo
+from gpiozero import AngularServo # For direct commands to servo
 
+# Board setup
+servoGpioPin = 18
+GPIO.setmode(GPIO.BCM) # BCM: Broadcom SOC channel
+GPIO.setup(servoGpioPin, GPIO.OUT)
+
+pin = GPIO.PWM(servoGpioPin, 50) # Set GPIO pin 17 to operate at 50Hz 
+pin.start(2.5) # Duty cycle for MG996R servo - units ms
+try:
+    while True:
+        pin.ChangeDutyCycle(5)
+        time.sleep(0.5)
+        pin.ChangeDutyCycle(7.5)
+        time.sleep(0.5)
+        pin.ChangeDutyCycle(10)
+        time.sleep(0.5)
+except KeyboardInterrupt:
+    pin.stop()
+    GPIO.cleanup()
+
+
+
+'''
 servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
 
 while (True):
@@ -12,3 +34,4 @@ while (True):
     time.sleep(2)
     servo.angle = -90
     time.sleep(2)
+'''
