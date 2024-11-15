2c14093d2bd78a88deebe6927dffca87186731de
damithtennakoon <damith.nasa@gmail.com>
2024-11-15 13:28:57 -0500

    testing pwm angles

--- src/actuators/scripts/servo_controller.py
+++ src/actuators/scripts/servo_controller.py
@@ -12,17 +12,22 @@ pin = GPIO.PWM(servoGpioPin, 50) # Set GPIO pin 17 to operate at 50Hz
 pin.start(2.5) # Duty cycle for MG996R servo - units ms
 try:
     while True:
-        pin.ChangeDutyCycle(5)
-        time.sleep(0.5)
-        pin.ChangeDutyCycle(7.5)
-        time.sleep(0.5)
-        pin.ChangeDutyCycle(10)
-        time.sleep(0.5)
+        #pin.ChangeDutyCycle(5) # 0
+        #time.sleep(2)
+        #pin.ChangeDutyCycle(7.5)
+        #time.sleep(2)
+        #pin.ChangeDutyCycle(10) #-90
+        #time.sleep(2)
+        pin.ChangeDutyCycle(5) # 0
+        time.sleep(2)
 except KeyboardInterrupt:
     pin.stop()
     GPIO.cleanup()
 
 
+# 0 = -90 pos
+# 10 = 0 pos
+#
 
 '''
 servo = AngularServo(18, min_pulse_width=0.0006, max_pulse_width=0.0023)
