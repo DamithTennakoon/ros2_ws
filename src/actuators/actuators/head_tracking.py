e2ebb88f24dc607c34ae31236de3900b46ec337f
DamithTennakoon <88393680+DamithTennakoon@users.noreply.github.com>
2024-11-25 13:32:51 -0500

    Servo actuator code - test 1

--- src/actuators/actuators/head_tracking.py
+++ src/actuators/actuators/head_tracking.py
@@ -15,10 +15,26 @@ from std_msgs.msg import Float64MultiArray
 
 # Import signal libraries
 import time
+import RPI.GPIO as GPIO
+import time
 
 # Import computational libraries
 import math
 import numpy as np
+
+# Function - compute the pulse width for the MG996R servo given an input target angle in degrees
+def angle_to_pulse_width(theta):
+    offset_pulse = 7.5 # Offset pulse width (0-degrees)
+    r_factor = 10/180 # pulse width (ms) per degree
+    pwm = theta*r_factor + offset_pulse # Output pulse
+    # Clamp the value between 2.5 and 12.5 
+    if (pwm > 12.5):
+        pwm = 12.5
+    elif (pwm < 2.5):
+        pwm = 2.5
+    else:
+        pwm = pwm
+    return pwm
 
 # Construct class
 class HeadTracking(Node):
@@ -26,6 +42,13 @@ class HeadTracking(Node):
     def __init__(self):
         super().__init__("head_tracking")
         self.get_logger().info("INITIALIZING HEAD TRACKING NODE")
+
+        # Initialize GPIO pins
+        self._yaw_servo_pin = 18 # BCM 18 (translates to pin #12)
+        GPIO.setmode(GPIO.BCM) # Set the GPIO pin mode to Broadcom SOC channel
+        GPIO.setup(self._yaw_servo_pin, GPIO.OUT) # Set the GPIO pin as an output channel
+        self._yaw_servo_pwm = GPIO.PWM(self._yaw_servo_pin, 50) # Set 50 Hz transmit frequency for BCM 18 (servo standard)
+        self._yaw_servo_pwm.start(0) # Set the starting pulse width to 0 (not actuating)
 
         # Create Publisher/Subscriber objects
         self._raw_data_subscriber = self.create_subscription(String, 'raw_input_data', self.parse_raw_data, 10) # Receive the string data from the Raw Input Data Topic
@@ -36,6 +59,15 @@ class HeadTracking(Node):
 
         # Define and execute callback functions
         self._actuate_servos_timer = self.create_timer(0.01, self.actuate_servos)
+
+    # Invoke desctructor - invoked at ROS node shutdown to cleanup GPIO board
+    def __del__(self):
+        # Cleanup resources
+        self.get_logger().info("CLEANING UP RESOURCES")
+        if hasattr(self, '_yaw_servo_pwm'):
+            self._yaw_servo_pin.stop()
+        GPIO.cleanup()
+        self.get_logger().info("GPIO CLEANUP COMPLETE")
 
     # Event Handler method - parse and store the HTC received data
     def parse_raw_data(self, msg):
@@ -48,7 +80,7 @@ class HeadTracking(Node):
 
     # Callback method - actuate the servo motors using HMD angles
     def actuate_servos(self):
-        self.get_logger().info(f"PITCH: {self._hmd_pitch}, YAW: {self._hmd_yaw}")
+        self._yaw_servo_pwm.ChangeDutyCycle(angle_to_pulse_width(self._hmd_yaw)) # Set the yaw servo angle to the HMD yaw angle
 
 # Create main method for looping the ROS node
 def main(args=None):
