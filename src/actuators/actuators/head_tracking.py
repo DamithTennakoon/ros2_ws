36ae62f51553c060e510fbc76d3f7a3f66c75045
DamithTennakoon <88393680+DamithTennakoon@users.noreply.github.com>
2024-11-25 12:42:07 -0500

    Casting and logging the HMD angles

--- src/actuators/actuators/head_tracking.py
+++ src/actuators/actuators/head_tracking.py
@@ -4,7 +4,8 @@
 # Writtern by: Damith Tennakoon
 
 # Notes:
-# - Define the Node
+# - Define the Node: x
+# - Debug the UDP server data: 
 
 # Import ROS2 libraries
 import rclpy
@@ -25,6 +26,29 @@ class HeadTracking(Node):
     def __init__(self):
         super().__init__("head_tracking")
         self.get_logger().info("INITIALIZING HEAD TRACKING NODE")
+
+        # Create Publisher/Subscriber objects
+        self._raw_data_subscriber = self.create_subscription(String, 'raw_input_data', self.parse_raw_data, 10) # Receive the string data from the Raw Input Data Topic
+
+        # Define variables for local data storage 
+        self._hmd_pitch =  0.0 # HMD pitch rotation angle
+        self._hmd_yaw = 0.0 # HMD yaw rotation angle
+
+        # Define and execute callback functions
+        self._actuate_servos_timer = self.create_timer(0.01, self.actuate_servos)
+
+    # Event Handler method - parse and store the HTC received data
+    def parse_raw_data(self, msg):
+        # Parse data if the received message is for HTC
+        if (msg.data[:3] == "HTC"):
+            split_string_list = msg.data.split(',') # Seperate the string using csv format
+            # Cast data type to float and store data
+            self._hmd_pitch = float(split_string_list[8]) # Index is i+1 due to index[0] value
+            self._hmd_yaw = float(split_string_list[9])
+
+    # Callback method - actuate the servo motors using HMD angles
+    def actuate_servos(self):
+        self.get_logger().info(f"PITCH: {self._hmd_pitch}, YAW: {self._hmd_yaw}")
 
 # Create main method for looping the ROS node
 def main(args=None):
