1a46a2f4cf984a105a4c74f442e0d1aa73df183e
DamithTennakoon <damith.nasa@gmail.com>
2025-06-11 16:37:40 -0400

    Stopped the joint angle data function from concatonating to tx

--- src/hri_comms/hri_comms/udp_server.py
+++ src/hri_comms/hri_comms/udp_server.py
@@ -47,7 +47,7 @@ class UdpServer(Node):
         self._udp_server.sendto(self._tx_data.encode('utf-8'), self._client_ip)
 
         # Debug to the console the transmitted data
-        self.get_logger().info(f"TX: {self._tx_data}")
+        #self.get_logger().info(f"TX: {self._tx_data}")
 
         # Publish raw input data
         msg = String()
@@ -60,7 +60,7 @@ class UdpServer(Node):
         joint_angles_string = [str(value) for value in msg.data]
 
         # Concatonate string array into a single variable
-        self._tx_data = ','.join(joint_angles_string)
+        #self._tx_data = ','.join(joint_angles_string)
 
     # Define a method to convert robot pose topic data into a transmittable string
     def parse_pose_data(self, msg):
