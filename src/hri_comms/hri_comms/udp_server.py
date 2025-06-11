5d63c44259b2e62e630c3e18d80c087a6904d35f
DamithTennakoon <damith.nasa@gmail.com>
2025-06-11 17:14:08 -0400

    timing issue check

--- src/hri_comms/hri_comms/udp_server.py
+++ src/hri_comms/hri_comms/udp_server.py
@@ -42,7 +42,7 @@ class UdpServer(Node):
 
         # Debug to the console the received data
         #self.get_logger().info(f"RX: {self._rx_data}")
-        self.get_logger().info(f"TX: {self._tx_data}")
+        #self.get_logger().info(f"TX: {self._tx_data}")
 
         # Transmit a message to the client
         self._udp_server.sendto(self._tx_data.encode('utf-8'), self._client_ip)
@@ -65,7 +65,7 @@ class UdpServer(Node):
         pose_string = [str(value) for value in msg.data] # Convert float values to a stringed array
         pose_string.insert(0, self._pose_datacode) # Insert the pose data code
         self._tx_data = ','.join(pose_string) # Set the transmit message variable to the pose data string seperated with a comma 
-        #self._tx_data = "TESTING TESTING 123"
+        self.get_logger().info(f"UPDATED TX DATA: {self._tx_data}")
 
 def main (args=None):
     rclpy.init(args=args)
