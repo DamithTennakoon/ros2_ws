f1db0c1f4b648f90808134ac16a707aeb46a8b7e
DamithTennakoon <damith.nasa@gmail.com>
2025-06-12 15:06:52 -0400

    for loop array

--- src/robot_arm_controller/robot_arm_controller/key_ee_ctrl.py
+++ src/robot_arm_controller/robot_arm_controller/key_ee_ctrl.py
@@ -130,10 +130,13 @@ class KeyEECtrl(Node):
     def tx_robot_arm(self):
         # Define the data type and msg variable, retrieve the pose, and store into publisher variable
         pose_msg = Float64MultiArray()
-        robot_coords = self._mc.get_coords()
+        raw_coords = self._mc.get_coords()
+        robot_coords = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        for i in range(len(raw_coords)):
+            robot_coords[i] = raw_coords[i]
         pose_msg.data = robot_coords
         self._publish_robot_pose.publish(pose_msg)
-        #self.get_logger().info(f"PUBLISH: {pose_msg.data}")
+        self.get_logger().info(f"PUBLISH: {pose_msg.data}")
 
 # Create main method for looping the ROS node
 def main(args=None):
