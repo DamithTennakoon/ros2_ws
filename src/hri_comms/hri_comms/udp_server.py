d8ae99a404b7f3b397a03a3abc37ccab128df03c
DamithTennakoon <damith.nasa@gmail.com>
2025-06-12 11:18:01 -0400

    Test if the problem is in the data parsing process of udp_server

--- src/hri_comms/hri_comms/udp_server.py
+++ src/hri_comms/hri_comms/udp_server.py
@@ -65,7 +65,7 @@ class UdpServer(Node):
         pose_string = [str(value) for value in msg.data] # Convert float values to a stringed array
         pose_string.insert(0, self._pose_datacode) # Insert the pose data code
         self._tx_data = ','.join(pose_string) # Set the transmit message variable to the pose data string seperated with a comma 
-        self.get_logger().info(f"UPDATED TX DATA: {self._tx_data}")
+        self.get_logger().info(f"UPDATED TX DATA: {msg.data}")
 
 def main (args=None):
     rclpy.init(args=args)
