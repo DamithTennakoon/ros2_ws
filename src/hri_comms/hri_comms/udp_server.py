5ae2006ad0c3410415203cc71d438f18bbc1ed9a
DamithTennakoon <damith.nasa@gmail.com>
2025-06-12 15:21:27 -0400

    test

--- src/hri_comms/hri_comms/udp_server.py
+++ src/hri_comms/hri_comms/udp_server.py
@@ -68,7 +68,7 @@ class UdpServer(Node):
         for i in range(len(pose_string)):
             self._tx_data += "," + pose_string[i]
 
-        #self._tx_data = ','.join(pose_string) + ',' + self._pose_datacode # Set the transmit message variable to the pose data string seperated with a comma 
+        self._tx_data = self._pose_datacode + ',' + ','.join(pose_string) # Set the transmit message variable to the pose data string seperated with a comma 
         self.get_logger().info(f"TX: {self._tx_data}")
 
 
