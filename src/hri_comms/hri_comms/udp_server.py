f7ce447e16de348b2a6566c323a41709366d1a9b
DamithTennakoon <damith.nasa@gmail.com>
2025-06-12 11:40:15 -0400

    udp_test
    testing to see if the error is with the concatonation

--- src/hri_comms/hri_comms/udp_server.py
+++ src/hri_comms/hri_comms/udp_server.py
@@ -63,9 +63,13 @@ class UdpServer(Node):
     # Define a method to convert robot pose topic data into a transmittable string
     def parse_pose_data(self, msg):
         pose_string = [str(value) for value in msg.data] # Convert float values to a stringed array
-        pose_string.insert(0, self._pose_datacode) # Insert the pose data code
-        self._tx_data = ','.join(pose_string) # Set the transmit message variable to the pose data string seperated with a comma 
-        self.get_logger().info(f"UPDATED TX DATA: {self._tx_data}")
+
+        # DEBUG
+        self._tx_data = pose_string
+        # --
+        #pose_string.insert(0, self._pose_datacode) # Insert the pose data code
+        #self._tx_data = ','.join(pose_string) # Set the transmit message variable to the pose data string seperated with a comma 
+        #self.get_logger().info(f"UPDATED TX DATA: {self._tx_data}")
 
 def main (args=None):
     rclpy.init(args=args)
